e404d15d7b5dc2f76154248d80e284cd7ac47d3a
theverygaming <clemibunge@gmail.com>
2022-05-09 00:41:30 +0200

    fft stuff

---
+++ fft.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <volk/volk.h>
+#include <fftw3.h>
+#include <complex>
+#include "window.h"
+
+namespace dsp::fft {
+    class complexPowerSpectrum {
+    public:
+        complexPowerSpectrum(int N) {
+            _N = N;
+            fft_window = (float*)fftwf_malloc(sizeof(float) * _N);
+            for (int i = 0; i < _N; i++) {
+                fft_window[i] = windowfunctions::blackman(i, _N - 1);
+            }
+            fftin = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * _N);
+            fftout = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * _N);
+            fftplan = fftwf_plan_dft_1d(_N, fftin, fftout, FFTW_FORWARD, FFTW_ESTIMATE);
+        }
+
+        ~complexPowerSpectrum() {
+            fftwf_free(fftin);
+            fftwf_free(fftout);
+            fftwf_free(fft_window);
+            fftwf_destroy_plan(fftplan);
+        }
+
+        void processFFT(std::complex<float>* in, float* out) {
+            volk_32fc_32f_multiply_32fc((lv_32fc_t*)fftin, in, fft_window, _N);
+            fftwf_execute(fftplan);
+            volk_32fc_s32f_power_spectrum_32f(out, (lv_32fc_t*)fftout, _N, _N);
+        }
+    private:
+        fftwf_complex* fftin;
+        fftwf_complex* fftout;
+        fftwf_plan fftplan;
+        float* fft_window;
+        int _N;
+    };
+}
