1e1905e9c262eecb1cfa1051ba9a5ad8c3fb2588
theverygaming <clemibunge@gmail.com>
2022-07-29 05:58:47 +0200

    QPSK stuff

--- modulator.h
+++ modulator.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <complex>
+#include "math.h"
 #include "vco.h"
+#include "firfilters.h"
 
 namespace dsp::modulator {
     class r2FSKmodulator {
@@ -14,6 +17,7 @@ namespace dsp::modulator {
             f1val = f1 / f0;
             vco.changeBaseFreq(f0, samplerate);
             symbolSamples = (1 / baudrate) * samplerate;
+            printf("dsp::modulator::r2FSKmodulator - actual baudrate: %f\n", (1 / (float)symbolSamples) * samplerate);
         }
 
         ~r2FSKmodulator() {
@@ -44,5 +48,76 @@ namespace dsp::modulator {
         float f1val = 0;
         float* sampleBuffer;
         unsigned int symbolSamples = 0; // the output baudrate may change slightly depending on what baudrate-samplerate combination is chosen! _this should definitely be fixed later_
+    };
+
+    class cQPSKmodulator {
+    public:
+        cQPSKmodulator(float symrate, int samplerate, int maxInputCount) {
+            changeParameters(symrate, samplerate, maxInputCount);
+        }
+
+        void changeParameters(float symrate, int samplerate, int maxInputCount) {
+            symbolSamples = (1 / symrate) * samplerate;
+            printf("dsp::modulator::cQPSKmodulator - actual symbolrate: %f\n", (1 / (float)symbolSamples) * samplerate);
+        }
+
+        ~cQPSKmodulator() {
+
+        }
+        
+        int calcOutSamples(int inCount) {
+            return ((inCount * 8) / 2) * symbolSamples;
+        }
+
+        // Expects raw bytes on the input
+        void process(char* in, int inCount, std::complex<float>* out) {
+            int counterSamples = 0;
+            int a = 0;
+            for(int i = 0; i < inCount; i++) {
+                for(int j = 0; j < 4; j++) {
+
+                    // differential encoder
+                    unsigned int newPhaseIndex = ((unsigned int)getBitPair(in[i], j) + lastPhaseIndex) % 4;
+                    lastPhaseIndex = newPhaseIndex;
+                    
+                    float PhaseShiftDeg = (float)phases[newPhaseIndex];
+
+                    float PhaseShiftRadians = PhaseShiftDeg * (FL_M_PI / 180);
+
+                    float reVal = cos(PhaseShiftRadians);
+                    float imVal = sin(PhaseShiftRadians);
+
+                    for(int k = 0; k < symbolSamples; k++) {
+                        out[counterSamples] = {reVal, imVal};
+                        counterSamples++;
+                    }
+                }
+            }
+        }
+
+    private:
+        unsigned int symbolSamples = 0;
+
+        unsigned char getBitPair(unsigned char in, int n) {
+            return (in >> (n * 2)) & 0x3;
+        }
+
+        /*
+         * +-------+-------+  +----+----+
+         * | 135°  | 45°   |  | 01 | 11 |
+         * +-------+-------+  +----+----+
+         * | 225°  | 315°  |  | 00 | 10 |
+         * +-------+-------+  +----+----+
+        */
+        int phases[4] = {45, 135, 225, 315};
+        unsigned int lastPhaseIndex = 0;
+
+        uint32_t differentialDecoder_last = 0;
+        uint32_t differentialDecoder(uint32_t in) {
+            uint32_t decoded = (in - differentialDecoder_last) % 4;
+            differentialDecoder_last = in;
+            return decoded;
+        }
+
     };
 }
--- resamplers.h
+++ resamplers.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "firfilters.h"
+#include <numeric>
 
 namespace dsp
 {
@@ -118,6 +119,101 @@ namespace dsp
 
         private:
             int _divider;
+        };
+
+        class PSK_PulseShaping_CCRationalResamplerBlock // satdump copypasted 
+        {
+            public:
+            PSK_PulseShaping_CCRationalResamplerBlock(int tapcount, unsigned int interpolation, float alpha, int maxInputSamples) {
+                unsigned int decimation = 1;
+                d_interpolation = interpolation;
+                d_decimation = decimation;
+
+                tapcount |= 1; // make sure tapcount is odd
+                
+                float *tapsr = (float*)malloc(tapcount * sizeof(float));
+                filters::FIRcoeffcalc::root_raised_cosine(1, interpolation, decimation, alpha, tapcount, tapsr);
+
+                int align = volk_get_alignment();
+
+                // Buffer
+                in_buffer = 0;
+                int size = 2 * maxInputSamples;
+                buffer = (std::complex<float> *)volk_malloc(size * sizeof(std::complex<float>), align);
+                std::fill(buffer, &buffer[size], 0);
+
+                // Start by reducing the interp and decim by their GCD
+                int gcd = std::gcd(interpolation, decimation);
+                d_interpolation /= gcd;
+                d_decimation /= gcd;
+                
+                // Filter number & tap number
+                nfilt = d_interpolation;
+                ntaps = tapcount / nfilt;
+
+                // If Ntaps is slightly over 1, add 1 tap
+                if (fmod(double(tapcount) / double(nfilt), 1.0) > 0.0)
+                    ntaps++;
+
+                // Init tap buffers
+                taps = (float **)volk_malloc(nfilt * sizeof(float *), align);
+                for (int i = 0; i < nfilt; i++)
+                {
+                    taps[i] = (float *)volk_malloc(ntaps * sizeof(float), align);
+                    memset(taps[i], 0, ntaps);
+                }
+
+                // Setup taps
+                for (int i = 0; i < tapcount; i++)
+                    taps[i % nfilt][(ntaps - 1) - (i / nfilt)] = tapsr[i];
+
+
+
+                free(tapsr);
+            }
+
+            ~PSK_PulseShaping_CCRationalResamplerBlock() {
+                for (int i = 0; i < nfilt; i++)
+                    volk_free(taps[i]);
+                volk_free(taps);
+                volk_free(buffer);
+            }
+
+            void process(std::complex<float> *input, std::complex<float> *output, int nsamples) {
+                memcpy(&buffer[ntaps], input, nsamples * sizeof(std::complex<float>));
+                in_buffer = ntaps + nsamples;
+
+                int outc = 0;
+                for (int i = 0; i < in_buffer - ntaps;)
+                {
+                    volk_32fc_32f_dot_prod_32fc((lv_32fc_t *)&output[outc++], (lv_32fc_t *)&buffer[i], taps[d_ctr], ntaps);
+                    
+                    d_ctr += d_decimation;
+                    while (d_ctr >= d_interpolation)
+                    {
+                        d_ctr -= d_interpolation;
+                        i++;
+                    }
+                }
+
+                memmove(&buffer[0], &buffer[in_buffer - ntaps], ntaps * sizeof(std::complex<float>));
+            }
+
+            
+            private:
+            // Settings
+            int d_interpolation;
+            int d_decimation;
+            int d_ctr = 0;
+
+            // Buffer
+            std::complex<float> *buffer;
+            int in_buffer;
+
+            // Taps
+            float **taps;
+            int nfilt; // Number of filters (one per phase)
+            int ntaps;
         };
     }
 }
